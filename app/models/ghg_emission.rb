85a8e1a99e48bb6cb2b0ff19748f9d9b582f8706
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2015-09-10 11:56:36 -0400

    emissions totals by factor method

--- app/models/ghg_emission.rb
+++ app/models/ghg_emission.rb
@@ -1,2 +1,8 @@
 class GhgEmission < ActiveRecord::Base
+
+  def self.totals_by_scenario_year_(factor=nil)
+    grouping_cols = [:scenario_id, :year]
+    grouping_cols << factor if factor
+    group(grouping_cols).sum(:total_emissions)
+  end
 end
