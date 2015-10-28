d5aa3bb65d8b80f850865e0941585c4365173fd3
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2015-10-28 11:01:36 -0400

    exclude transportation numbers in census tract totals and some refactoring

--- app/models/census_tract.rb
+++ app/models/census_tract.rb
@@ -2,9 +2,16 @@ class CensusTract < ActiveRecord::Base
 
   has_many :ghg_emissions, primary_key: :zone_id, foreign_key: :zone_id, inverse_of: :census_tract
 
-  def self.with_emissions_totals_where_year_scenario(year, scenario_id, other={})
-    where = other.merge({year: year, scenario_id: scenario_id})
-    subquery = GhgEmission.yearly_totals_by_factors_query([:zone], where).as('ghg_totals')
+  def self.with_emissions_totals_where_year_scenario(year, scenario_id, other_where_eq_in={})
+    # build GhgEmission subquery that will be joined
+    where = other_where_eq_in.merge({year: year, scenario_id: scenario_id})
+    subquery = yearly_emissions_totals_by_zones_query(where)
+
+    # transportation sector totals do not map meaninfgully and should be removed
+    subquery = subquery.where(
+                  emission_t[:sector_id].
+                  not_eq(sector_id_for('Transportation'))
+                ).as('ghg_totals')
 
     query = t.project(Arel.star).
               join(subquery).
@@ -15,8 +22,24 @@ class CensusTract < ActiveRecord::Base
 
 private
 
+  def self.sector_id_for(name)
+    Sector.find_by_name(name).id
+  end
+
+  def self.yearly_emissions_totals_by_zones_query(where_clause)
+    emission_model.yearly_totals_by_factors_query([:zone], where_clause)
+  end
+
+  def self.emission_model
+    GhgEmission
+  end
+
   def self.t
     arel_table
+  end
+
+  def self.emission_t
+    emission_model.arel_table
   end
 
 end
--- app/models/sector.rb
+++ app/models/sector.rb
@@ -2,4 +2,7 @@ class Sector < ActiveRecord::Base
 
   has_many :ghg_emissions
 
+  def self.find_by_name(name)
+    find_by(name: name)
+  end
 end
