7fa8802f421e7513a1192b022a85887d3f440fd8
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2015-11-18 13:08:29 -0500

    elaborating JS Util module

--- app/assets/javascripts/util.js
+++ app/assets/javascripts/util.js
@@ -1,19 +1,34 @@
-var Util = {
+var Util = function() {
 
-  nextTens: function(n) {
-    var next = Math.ceil(n/10) * 10;
+  function ceilTens(n) {
+    return(Math.ceil(n/10) * 10);
+  };
+
+  function nextTens(n) {
+    var next = ceilTens(n);
     if (next === n) {
       next = n + 10;
     }
     return(next);
-  },
+  };
 
-  previousTens: function(n) {
-    var previous = Math.floor(n/10) * 10;
+  function floorTens(n) {
+    return(Math.floor(n/10) * 10);
+  }
+
+  function previousTens(n) {
+    var previous = floorTens(n);
     if (previous === n) {
       previous = n - 10;
     }
     return(previous);
-  }
+  };
 
-};
+  return {
+    ceilTens: ceilTens,
+    nextTens: nextTens,
+    floorTens: floorTens,
+    previousTens: previousTens
+  };
+
+}();
