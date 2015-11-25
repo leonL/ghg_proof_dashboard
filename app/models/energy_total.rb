6b35971340da8ac89d9ab9453e1c4f057994e275
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2015-11-25 09:54:53 -0500

    minor text re-format

--- app/models/energy_total.rb
+++ app/models/energy_total.rb
@@ -4,37 +4,37 @@ class EnergyTotal < ActiveRecord::Base
   belongs_to :fuel_type
   belongs_to :sector
 
-    def self.yearly_totals_by_factors(factors=[], where_vals={})
-      query = yearly_totals_by_factors_query(factors, where_vals)
-      records = find_by_sql(query)
-      preloader.preload(records, factors)
-      records
+  def self.yearly_totals_by_factors(factors=[], where_vals={})
+    query = yearly_totals_by_factors_query(factors, where_vals)
+    records = find_by_sql(query)
+    preloader.preload(records, factors)
+    records
+  end
+
+  def self.yearly_totals_by_factors_query(factors=[], where_vals={}, order_by_total=false)
+    factor_cols = factors.map{|col_name| t["#{col_name}_id"]}
+    factor_cols.unshift t[:year]
+
+    order_cols = order_by_total ? 'total' : factor_cols
+
+    query = t.project(factor_cols, t[:total].sum.as('total')).
+      group(factor_cols).
+      order(order_cols)
+
+    where_vals.each do |col_name, val|
+      query = val.is_a?(Array) ? query.where(t[col_name].in(val)) : query.where(t[col_name].eq(val))
     end
+    query
+  end
 
-    def self.yearly_totals_by_factors_query(factors=[], where_vals={}, order_by_total=false)
-      factor_cols = factors.map{|col_name| t["#{col_name}_id"]}
-      factor_cols.unshift t[:year]
+private
 
-      order_cols = order_by_total ? 'total' : factor_cols
+  def self.t
+    arel_table
+  end
 
-      query = t.project(factor_cols, t[:total].sum.as('total')).
-        group(factor_cols).
-        order(order_cols)
-
-      where_vals.each do |col_name, val|
-        query = val.is_a?(Array) ? query.where(t[col_name].in(val)) : query.where(t[col_name].eq(val))
-      end
-      query
-    end
-
-  private
-
-    def self.t
-      arel_table
-    end
-
-    def self.preloader
-      ActiveRecord::Associations::Preloader.new
-    end
+  def self.preloader
+    ActiveRecord::Associations::Preloader.new
+  end
 
 end
